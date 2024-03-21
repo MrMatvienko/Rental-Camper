eff3a640b475d6668172703f97a302d2cd2d44e4
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2024-03-21 16:11:39 +0200

    filter conrainer

--- src/componets/Filter/Filter.jsx
+++ src/componets/Filter/Filter.jsx
@@ -1,11 +1,15 @@
+import SearchBtn from "../Button/SearchBtn/SearchBtn";
 import Equipment from "./Equipment/Equipment";
 import Location from "./Location/Location";
+import VehicleTypeType from "./VehicleType/VehicleType";
 
 const Filter = () => {
   return (
     <>
       <Location />
       <Equipment />
+      <VehicleTypeType />
+      <SearchBtn />
     </>
   );
 };
